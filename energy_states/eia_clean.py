5dfa362cb4ca6fd922302bde9d6176ea9f0fda6c
John Morris Christenson <jchristenson@linux4.cs.uchicago.edu>
2023-03-02 19:14:36 -0600

    progress

--- energy_states/eia_clean.py
+++ energy_states/eia_clean.py
@@ -1,34 +1,40 @@
 import pandas as pd
 import pathlib
+from .states_dict import us_states_anddc
+
+
+def clean_consumed_data():
+    """
+    """
+    consumed_filename = pathlib.Path(__file__).parent / """eia_states_
+                data/TotalEnergyConsumedperCapita-StateRankings.csv"""
+    
+    return clean(consumed_filename) 
+
 
 def clean_emissions_data():
     """
     """
-    emissions_filename = pathlib.Path(__file__).parent / """eia_states_data/TotalCarbonDioxideEmissions-StateRankings.csv"""
+    emissions_filename = pathlib.Path(__file__).parent / """eia_states_
+                data/TotalCarbonDioxideEmissions-StateRankings.csv"""
     
     return clean(emissions_filename)
-    
-        
-def clean_consumed_data():
-    """
-    """
-    consumed_filename = pathlib.Path(__file__).parent / """data/TotalEnergyConsumedperCapita-StateRankings.csv"""
-    
-    return clean(consumed_filename) 
+
         
 def clean_expenditures_data():
     """
     """
-    expenditures_filename = pathlib.Path(__file__).parent / """data/
-                        TotalEnergyExpendituresperCapita-StateRankings.csv"""
+    expenditures_filename = pathlib.Path(__file__).parent / """eia_states_
+                data/TotalEnergyExpendituresperCapita-StateRankings.csv"""
     
     return clean(expenditures_filename)
-    
+
+
 def clean_production_data():
     """
     """
-    production_filename = pathlib.Path(__file__).parent / """data/
-                        TotalEnergyProduction-StateRankings.csv"""
+    production_filename = pathlib.Path(__file__).parent / """eia_states_
+                data/TotalEnergyProduction-StateRankings.csv"""
     
     return clean(production_filename)
 
@@ -37,21 +43,24 @@ def clean(filename):
     """
     """
     data_to_clean = pd.read_csv(filename)
-    
     data = data_to_clean.copy() 
-    
-    percentage_category_name = "Percentage of U.S. " + data.columns[2].split(
-                        ',', 1)[0]
     
     total_of_category = data[data.columns[2]].sum()
 
-    data.drop(data[~data["State"].isin(["IL", "TX"])].index, inplace=True)
-    
+    data.drop(data[~data["State"].isin(["PA", "TX"])].index, inplace=True)
     data = data.drop(data.columns[3], axis=1) 
     
-    data = data.sort_values("State")
-    
+    percentage_category_name = "Percentage of U.S. " + data.columns[2].split(
+                ',', 1)[0]
     data[percentage_category_name] = (data[data.columns[2]] / 
-                        total_of_category) * 100
+                total_of_category) * 100
+    
+    data[["Rank", "State"]] = data.apply(lambda row: 
+                ["State Ranked <b>{}</b> out of 51".format(row["Rank"]),
+                us_states_anddc[row["State"]]], axis = 1, 
+                result_type = "expand"
+)
+ 
+    data = data.sort_values("State")
     
     return data 
--- energy_states/energy_dataviz.py
+++ energy_states/energy_dataviz.py
@@ -1,35 +1,72 @@
 from .eia_clean import clean_emissions_data, clean_consumed_data
 from .eia_clean import clean_expenditures_data, clean_production_data
-import pandas as pd
 import plotly.express as px
 
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# import numpy as np
 
-def create_graph():
+def consumed_graph():
     """
     """
+    title_section = "<b>Energy Consumed by Pennsylvania & Texas</b>"
     
+    bar_graph = (clean_consumed_data(), clean_consumed_data().columns[2], 
+            clean_consumed_data().columns[3], title_section
+)
     
-    # graph = sns.barplot(x=  clean_emissions_data().columns[1], 
-    #                 y=clean_emissions_data().columns[2], data=clean_emissions_data(),
-    #                 errwidth=0)
+    return bar_graph
 
-    # for rank in clean_emissions_data().columns[0]:
-    #     graph.bar_label(rank,) 
-    
-    
-    # # graph = sns.barplot(x=  clean_emissions_data()[clean_emissions_data().columns[1]], 
-    # #                 y=clean_emissions_data()[clean_emissions_data().columns[2]], data=clean_emissions_data(),
-    # #                 errwidth=0)
 
-    # # for rank in clean_emissions_data()[clean_emissions_data().columns[0]]:
-    # #     graph.bar_label(rank,)
+def emissions_graph():
+    """
+    """
+    title_section =  "<b>Carbon Dioxide Emissions from Pennsylvania & Texas</b>"
     
-    # fig = graph.get_figure()
-    # fig.savefig("output.png")
-    # print(graph)
-    # return graph
+    bar_graph = (clean_emissions_data(), clean_emissions_data().columns[3], 
+            clean_emissions_data().columns[2], title_section
+)
+    
+    return bar_graph
 
-    # data[data.columns[2]]
+
+def expenditures_graph():
+    """
+    """
+    title_section = "<b>Energy Expenditures for Pennsylvania & Texas</b>"
+    
+    bar_graph = (clean_expenditures_data(), clean_expenditures_data().columns[3]
+            , clean_expenditures_data().columns[2], title_section
+)
+    
+    return bar_graph
+
+
+def production_graph():
+    """
+    """
+    title_section = "<b>Energy Production by Pennsylvania & Texas</b>"
+    
+    bar_graph = (clean_production_data(), clean_production_data().columns[2], 
+            clean_production_data().columns[3], title_section
+)
+    
+    return bar_graph
+
+
+def create_graph(data, y_variable, added_hover_variable, title_section):
+    """
+    """
+    fig = px.bar(data, x = "State", y = y_variable, text = "Rank",  
+            color = "State", color_discrete_sequence = ["mediumpurple", "red"], 
+            pattern_shape = "State", pattern_shape_sequence=["+", "x"],
+            hover_data = [added_hover_variable]
+)
+    
+    fig.update_traces(textposition = 'outside')
+    
+    fig.update_layout(showlegend = False,  font = dict(size = 17),
+            title = {"text": title_section
+            , "y": 0.96, "x": 0.5, "xanchor": "center", "yanchor": "top"},     
+)
+    
+    fig.show()
+    
+    return fig
