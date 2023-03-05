e9719af054f7ef049b8f82e74a914ac46e1fd6bf
pablomonhel <58742231+pablomonhel@users.noreply.github.com>
2023-03-04 19:50:49 -0600

    Design and input changes

--- cody_bills/app.py
+++ cody_bills/app.py
@@ -6,11 +6,15 @@ import plotly.express as px
 import dash_bootstrap_components as dbc
 from dash import Dash, html, dcc, Input, Output, get_asset_url, dash_table
 
-with open("cody_bills/assets/table_pennsylvania.json", "r") as file:
-    datatable_pennsylvania = json.load(file)
+datatable_pennsylvania = pd.read_csv("cody_bills/assets/table_pennsylvania.txt").to_dict("records")
+datatable_texas = pd.read_csv("cody_bills/assets/table_texas.txt").to_dict("records")
 
-with open("cody_bills/assets/table_texas.json", "r") as file:
-    datatable_texas = json.load(file)
+total_energy_produced = pd.read_csv("cody_bills/assets/total_energy_production.txt")
+total_carbon_dioxide = pd.read_csv("cody_bills/assets/total_carbon_dioxide.txt")
+capita_energy_expenditure = pd.read_csv("cody_bills/assets/per_capita_energy_expenditure.txt")
+capita_energy_consumed = pd.read_csv("cody_bills/assets/per_capita_energy_consumed.txt")
+
+
 # SPACELAB
 app = Dash(external_stylesheets=[dbc.themes.SIMPLEX])
 app.layout = html.Div([
@@ -32,34 +36,7 @@ app.layout = html.Div([
     ]),
 
     dbc.Card(
-        dbc.CardBody([
-            dbc.Row([
-                dbc.Col([
-
-                    html.H3("Histograms - Energy Policy Index", style = {'textAlign': 'center'}),
-
-                    dcc.Dropdown(
-                        options = ["Pennsylvania", "Texas"],
-                        value = "Pennsylvania",
-                        id = "histogram-dropdown",
-                        style={'width': '100%'}
-                    ),
-
-                    html.P("""
-                        The histograms show the distribution of the Energy Policy
-                        Index, for each state.
-                    
-                    """, style = {'textAlign': 'center'}),
-                ], width = 4),
-
-                dbc.Col([
-                    dcc.Graph(
-                        id = "histogram-graph")
-                        # id = "histogram-graph")], style={'width': '70%', 'display': 'inline-block'})
-                    
-                ], width=8)
-            ], align='center'),
-            
+        dbc.CardBody([            
             dbc.Row([
                 dbc.Col([
                     html.H3("Tables - Bills Metadata and Index", style = {'textAlign': 'center'}),
@@ -87,12 +64,55 @@ app.layout = html.Div([
                         page_size = 15,
                         fixed_rows = {'headers': True},
                         # filter_action='native'
-                        style_cell={"whiteSpace": "pre-line"},
-                        style_data = {'minWidth': '150px', 'width': '180px', 'maxWidth': '230px', 'height': 'auto', 'overflowY': 'auto'} ,
-                        # style_table = {'width': '75%', 'height': '300px', 'overflowY': 'auto'})
+                        style_cell = {"whiteSpace": "pre-line", 'textAlign': 'left'},
+                        style_table={'minWidth': '100%'},
+                        style_data = {'minWidth': '100px', 'maxWidth': '400px', 'height': 'auto', 'overflowY': 'auto'} ,
+                        # style_table = {'width': '75%', 'overflowY': 'auto'},
+                        fill_width=False,
+                        style_cell_conditional = [
+                            {'if': {'column_id': 'Description'},
+                            'width': '400px'},
+                            {'if': {'column_id': 'Chamber'},
+                            'width': '100px'},
+                            {'if': {'column_id': 'Created Date'},
+                            'width': '200px%'},
+                            {'if': {'column_id': 'Energy Policy Index'},
+                            'width': '200px%'},
+                            {'if': {'column_id': 'url'},
+                            'width': '100px%'}
+
+                        ]                    
+                    
                     )
                     
                 ], width = 8)
+            ], align='center'),
+
+            dbc.Row([
+                dbc.Col([
+
+                    html.H3("Histograms - Energy Policy Index", style = {'textAlign': 'center'}),
+
+                    dcc.Dropdown(
+                        options = ["Pennsylvania", "Texas"],
+                        value = "Pennsylvania",
+                        id = "histogram-dropdown",
+                        style={'width': '100%'}
+                    ),
+
+                    html.P("""
+                        The histograms show the distribution of the Energy Policy
+                        Index, for each state.
+                    
+                    """, style = {'textAlign': 'center'}),
+                ], width = 4),
+
+                dbc.Col([
+                    dcc.Graph(
+                        id = "histogram-graph")
+                        # id = "histogram-graph")], style={'width': '70%', 'display': 'inline-block'})
+                    
+                ], width=8)
             ], align='center'),
 
             dbc.Row([
@@ -137,8 +157,11 @@ app.layout = html.Div([
                 dbc.Col([
                     html.H3("Energy and CO2 Emission Barcharts", style = {'textAlign': 'center'}),
                     dcc.Dropdown(
-                        options = ["Total Energy - Total Carbon Dioxide", "Energy Consumed - Energy Expenditure"],
-                        value = "Total Energy - Total Carbon Dioxide",
+                        options = ["Percentage of U.S. Total Energy Production", 
+                                    "Percentage of U.S. Total Carbon Dioxide Emissions", 
+                                    "Energy Expenditure Per Capita", 
+                                    "Energy Consumed Per Capita"],
+                        value = "Percentage of U.S. Total Energy Production",
                         id = "barchart-dropdown",
                         style={'width': '100%'}
                         )
@@ -148,12 +171,7 @@ app.layout = html.Div([
                 dbc.Col([
                     dcc.Graph(
                         id = "barchart-graph-totals")], 
-                        width=4),
-
-                dbc.Col([
-                    dcc.Graph(
-                        id = "histogram-graph-percapita")], 
-                        width=4),
+                        width=8),
 
             ], align='center')
 
@@ -230,17 +248,53 @@ def display_wordclouds_texas(ngram_type):
 
     return 'data:image/png;base64,{}'.format(encoded_image.decode())
 
-# @app.callback(
-#     Output(component_id = "barchart-graph-totals", component_property = "figure"),
-#     Input(component_id = "barchart-dropdown", component_property = "value")
-# )
-# def graph_barchart(subjects):
+@app.callback(
+    Output(component_id = "barchart-graph-totals", component_property = "figure"),
+    Input(component_id = "barchart-dropdown", component_property = "value")
+)
+def graph_barchart(subject):
+    if subject == "Percentage of U.S. Total Energy Production":
+        data = total_energy_produced
+        y_variable = "Percentage of U.S. Total Energy Production"
+        added_hover_variable = "Percentage of U.S. Total Energy Production"
+        title_section = "<b>Percengage Energy Production by Pennsylvania & Texas</b>"
+
+    elif subject == "Percentage of U.S. Total Carbon Dioxide Emissions":
+        data = total_carbon_dioxide
+        y_variable = "Percentage Carbon Dioxide Emissions"
+        added_hover_variable = "Percentage of U.S. Total Carbon Dioxide Emissions"
+        title_section = "<b>Carbon Dioxide Emissions from Pennsylvania & Texas</b>"
+
+    elif subject == "Energy Expenditure Per Capita":
+        data = capita_energy_expenditure
+        y_variable = "Total Energy Expenditures per Capita, $"
+        added_hover_variable = "Percentage of U.S. Total Energy Expenditures per Capita"
+        title_section = "<b>Energy Expenditures for Pennsylvania & Texas</b>"
+
+    else:
+        data = capita_energy_consumed
+        y_variable = "Total Energy Consumed per Capita, million Btu"
+        added_hover_variable = "Percentage of U.S. Total Energy Consumed per Capita"
+        title_section = "<b>Energy Consumed by Pennsylvania & Texas</b>"
     
+    fig = px.bar(data, x = "State", y = y_variable, text = "Rank",  
+        color = "State", color_discrete_sequence = ["mediumpurple", "red"], 
+        pattern_shape = "State", pattern_shape_sequence=["+", "x"],
+        hover_data = [added_hover_variable]
+        )
+
+    fig.update_traces(textposition = 'outside')
+    
+    fig.update_layout(showlegend = False,  font = dict(size = 17),
+            title = {"text": title_section
+            , "y": 0.96, "x": 0.5, "xanchor": "center", "yanchor": "top"}
+            )
+
+    return fig
 
 
-app.run_server()
+
+app.run_server(port = 38456)
 
 # poetry add -dev (wordcloud, preprocessing, etc.)
 
-
-# , style = {'height': '100vh', 'justify-content': 'center', 'align-items': 'center'}
